1fb33e37873476a86dabbe58b192a32c483fc751
saimalam843 <saimalam843@gmail.com>
2025-03-07 21:47:59 +0500

    Saim's update to repo

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
-FROM python:3.8-slim
+FROM python:3.12
 
 WORKDIR /app
-COPY . /app
 
-RUN pip install -r requirements.txt
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
 
 EXPOSE 5000
+
+ENV FLASK_APP=app.py
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, MLOps!"
 
-@app.route('/abd')
-def abd():
-    return "hello im abdullah bin faisal"
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Flask App!"})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
